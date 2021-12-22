67ef157ed0950f927cef380789635743f5e129e0
snaztoz <hafidhmn@gmail.com>
2021-12-22 21:43:26 +0700

    Rename variable

--- src/app.py
+++ src/app.py
@@ -7,10 +7,10 @@ from . import algorithm, request
 app = FastAPI()
 
 @app.post("/{alg}/generate-key", status_code=fastapi.status.HTTP_200_OK)
-def generate_key(alg: str, response: Response):
+def generate_key(alg: str, res: Response):
     err = request.validate_algorithm(alg)
     if err is not None:
-        response.status_code = fastapi.status.HTTP_400_BAD_REQUEST
+        res.status_code = fastapi.status.HTTP_400_BAD_REQUEST
         return {"err": err}
 
     (pb, pv) = algorithm.get_keys(alg)
