52d51e8473262596acb901959918a95a63cac3a3
snaztoz <hafidhmn@gmail.com>
2021-12-22 21:03:08 +0700

    Attach RSA keys generator to API

--- src/algorithm.py
+++ src/algorithm.py
@@ -1,8 +1,23 @@
+from .rsa import AppRSA
+from .types import PublicKey, PrivateKey
+
+
 SUPPORTED_ALGORITHMS = [
     "rsa",
     "ecc",
     "ntru",
 ]
 
+
 def is_supported(alg: str) -> bool:
     return alg in SUPPORTED_ALGORITHMS
+
+
+def get_keys(alg: str) -> (PublicKey, PrivateKey):
+    keys_generator = {
+        "rsa": lambda: AppRSA.generate_keys(),
+        "ecc": lambda: ("", ""),
+        "ntru": lambda: ("", ""),
+    }
+
+    return keys_generator[alg]()
--- src/app.py
+++ src/app.py
@@ -1,18 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+import fastapi
 
 from . import algorithm, request
 
 
 app = FastAPI()
 
-@app.post("/{alg}/generate-key")
-def generate_key(alg: str):
+@app.post("/{alg}/generate-key", status_code=fastapi.status.HTTP_200_OK)
+def generate_key(alg: str, response: Response):
     err = request.validate_algorithm(alg)
     if err is not None:
+        response.status_code = fastapi.status.HTTP_400_BAD_REQUEST
         return err
 
+    (pb, pv) = algorithm.get_keys(alg)
+
     return {
-        "res": f"Key for {alg.upper()} is successfully generated"
+        "keys": {
+            "public": pb,
+            "private": pv
+        }
     }
 
 @app.post("/{alg}/encrypt")
