98dfc1e85847d14d63cf194b854a8f7e61675bdb
snaztoz <hafidhmn@gmail.com>
2021-12-22 21:42:20 +0700

    Create EncryptionRequest and key format validator

--- src/request.py
+++ src/request.py
@@ -5,13 +5,24 @@ from pydantic import BaseModel
 from . import algorithm
 
 
-class KeyGenerationRequest(BaseModel):
-    with_private_key: Optional[bool] = False
+class EncryptionRequest(BaseModel):
+    message: str
+    public_key: str
+
+    def get_e(self):
+        self.public_key.split()[0]
+
+    def get_n(self):
+        self.public_key.split()[1]
 
 
-def validate_algorithm(alg: str) -> (str):
-    err = None
+def validate_algorithm(alg: str) -> Optional[str]:
     if not algorithm.is_supported(alg):
         supported = ', '.join(algorithm.SUPPORTED_ALGORITHMS)
-        err = {"err": f"Unsupported algorithm: {alg}. Supported algorithms are: {supported}"}
-    return err
+        return f"Unsupported algorithm: {alg}. Supported algorithms are: {supported}"
+
+
+def validate_key_format(key: str) -> Optional[str]:
+    key = key.split()
+    if len(key) != 2 or not key[0].isdigit() or not key[1].isdigit():
+        return "invalid key format"
