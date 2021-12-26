91c24fb8f4c37bc6c8a79fdefa7bc7e91ba7ee68
snaztoz <hafidhmn@gmail.com>
2021-12-26 14:26:48 +0700

    Refactor

--- src/algorithm.py
+++ src/algorithm.py
@@ -9,36 +9,24 @@ SUPPORTED_ALGORITHMS = [
     "ntru",
 ]
 
+ALGORITHM_CLASS = {
+    "rsa": AppRSA,
+    "ecc": AppECC,
+    "ntru": None
+}
+
 
 def is_supported(alg: str) -> bool:
     return alg in SUPPORTED_ALGORITHMS
 
 
 def get_keys(alg: str) -> (PublicKey, PrivateKey):
-    keys_generator = {
-        "rsa": lambda: AppRSA.generate_keys(),
-        "ecc": lambda: AppECC.generate_keys(),
-        "ntru": lambda: ("", ""),
-    }
-
-    return keys_generator[alg]()
+    return ALGORITHM_CLASS[alg].generate_keys()
 
 
 def encrypt(alg: str, message: str, pb: PublicKey) -> str:
-    encryptor = {
-        "rsa": lambda: AppRSA.encrypt,
-        "ecc": lambda: AppECC.encrypt,
-        "ntru": lambda: ("", ""),
-    }
-
-    return encryptor[alg]()(message, pb)
+    return ALGORITHM_CLASS[alg].encrypt(message, pb)
 
 
 def decrypt(alg: str, message: str, pv: PrivateKey) -> str:
-    decryptor = {
-        "rsa": lambda: AppRSA.decrypt,
-        "ecc": lambda: AppECC.decrypt,
-        "ntru": lambda: ("", ""),
-    }
-
-    return decryptor[alg]()(message, pv)
+    return ALGORITHM_CLASS[alg].decrypt(message, pv)
